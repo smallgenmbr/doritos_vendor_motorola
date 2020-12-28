14980c74c190f7efc5a3fbe1d2ff62caffd329bf
Deivid21 <david.parra.ignacio@gmail.com>
2020-12-27 22:25:26 -0300

    sdm660-common: add Moto Radio FM
    Signed-off-by: Deivid21 <david.parra.ignacio@gmail.com>

--- sdm660-common/sdm660-common-vendor.mk
+++ sdm660-common/sdm660-common-vendor.mk
@@ -727,9 +727,31 @@ PRODUCT_COPY_FILES += \
     vendor/motorola/sdm660-common/proprietary/vendor/lib64/vendor.dolby.hardware.dms@1.0-impl.so:$(TARGET_COPY_OUT_VENDOR)/lib64/vendor.dolby.hardware.dms@1.0-impl.so \
     vendor/motorola/sdm660-common/proprietary/vendor/lib64/vendor.dolby.hardware.dms@1.0.so:$(TARGET_COPY_OUT_VENDOR)/lib64/vendor.dolby.hardware.dms@1.0.so
 
+# Moto FM Radio
+PRODUCT_COPY_FILES += \
+    vendor/motorola/sdm660-common/proprietary/framework/qcom.fmradio.jar:$(TARGET_COPY_OUT_SYSTEM)/framework/qcom.fmradio.jar \
+    vendor/motorola/sdm660-common/proprietary/lib/libbtconfigstore.so:$(TARGET_COPY_OUT_SYSTEM)/lib/libbtconfigstore.so \
+    vendor/motorola/sdm660-common/proprietary/lib/libqcomfm_jni.so:$(TARGET_COPY_OUT_SYSTEM)/lib/libqcomfm_jni.so \
+    vendor/motorola/sdm660-common/proprietary/lib64/libbtconfigstore.so:$(TARGET_COPY_OUT_SYSTEM)/lib64/libbtconfigstore.so \
+    vendor/motorola/sdm660-common/proprietary/lib64/libqcomfm_jni.so:$(TARGET_COPY_OUT_SYSTEM)/lib64/libqcomfm_jni.so \
+    vendor/motorola/sdm660-common/proprietary/product/etc/permissions/com.motorola.software.fmradioservice.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/permissions/com.motorola.software.fmradioservice.xml \
+    vendor/motorola/sdm660-common/proprietary/product/framework/qcom.fmradio_product.jar:$(TARGET_COPY_OUT_PRODUCT)/framework/qcom.fmradio_product.jar \
+    vendor/motorola/sdm660-common/proprietary/product/lib/libqcomfm_jni_product.so:$(TARGET_COPY_OUT_PRODUCT)/lib/libqcomfm_jni_product.so \
+    vendor/motorola/sdm660-common/proprietary/product/lib/vendor.qti.hardware.btconfigstore@1.0.so:$(TARGET_COPY_OUT_PRODUCT)/lib/vendor.qti.hardware.btconfigstore@1.0.so \
+    vendor/motorola/sdm660-common/proprietary/product/lib64/libqcomfm_jni_product.so:$(TARGET_COPY_OUT_PRODUCT)/lib64/libqcomfm_jni_product.so \
+    vendor/motorola/sdm660-common/proprietary/product/lib64/vendor.qti.hardware.btconfigstore@1.0.so:$(TARGET_COPY_OUT_PRODUCT)/lib64/vendor.qti.hardware.btconfigstore@1.0.so \
+    vendor/motorola/sdm660-common/proprietary/vendor/bin/init.qti.fm.sh:$(TARGET_COPY_OUT_VENDOR)/bin/init.qti.fm.sh \
+    vendor/motorola/sdm660-common/proprietary/vendor/etc/init/init.qti.fm.rc:$(TARGET_COPY_OUT_VENDOR)/etc/init/init.qti.fm.rc \
+    vendor/motorola/sdm660-common/proprietary/vendor/lib/vendor.qti.hardware.btconfigstore@1.0.so:$(TARGET_COPY_OUT_VENDOR)/lib/vendor.qti.hardware.btconfigstore@1.0.so \
+    vendor/motorola/sdm660-common/proprietary/vendor/lib/hw/vendor.qti.hardware.btconfigstore@1.0-impl.so:$(TARGET_COPY_OUT_VENDOR)/lib/hw/vendor.qti.hardware.btconfigstore@1.0-impl.so \
+    vendor/motorola/sdm660-common/proprietary/vendor/lib64/vendor.qti.hardware.btconfigstore@1.0.so:$(TARGET_COPY_OUT_VENDOR)/lib64/vendor.qti.hardware.btconfigstore@1.0.so \
+    vendor/motorola/sdm660-common/proprietary/vendor/lib64/hw/vendor.qti.hardware.btconfigstore@1.0-impl.so:$(TARGET_COPY_OUT_VENDOR)/lib64/hw/vendor.qti.hardware.btconfigstore@1.0-impl.so
+
 PRODUCT_PACKAGES += \
     MotoDolbyV3 \
     daxService \
+    FMPlayer \
+    FMRadioService \
     libdsi_netctrl \
     libgpustats \
     libqmi_cci \
